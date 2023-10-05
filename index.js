68e22534b42dc6a06bf722cb7bf6cd6427aa8fca
root <root@DESKTOP-598GE48.localdomain>
2023-10-05 15:17:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Define saturdayFun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define mondayWork function
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
